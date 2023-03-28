0bb727b8fa75c51b62190566e8c0833f0a2f2431
Michael Krukar <krukar@gmail.com>
2023-03-28 17:20:49 -0400

    Added meta properties

--- app/layouts/App.tsx
+++ app/layouts/App.tsx
@@ -8,8 +8,8 @@ const Layout = ({ children }: any) => {
             <Head>
                 <meta property="og:title" content="Krukar" />
                 <meta property="og:description" content="Toronto Full Stack Consultant/New Media Artist" />
-                {/* <meta property="og:image" content={children.props.meta.image} /> */}
-                {/* <meta name="twitter:card" content="summary_large_image" /> */}
+                <meta property="og:image" content="/meta.jpg" />
+                <meta name="twitter:card" content="summary_large_image" />
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://michaelkrukar.com" />
 
